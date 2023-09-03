a62a08330bd55ad2ca5cf1dad112e904e60fe7f3
Arpitam Saha <arpitam972@gmail.com>
2023-09-03 11:48:52 +0530

    added check balance and deposit

--- atm.java
+++ atm.java
@@ -4,30 +4,51 @@ import java.util.Scanner;
 public class atm {
     static Scanner sc = new Scanner(System.in);
 
-    static HashMap<String, Integer> hm = new HashMap<>();
+    static HashMap<String, Integer> accCred = new HashMap<>();
+    static HashMap<String, Double> accBal = new HashMap<>();
 
-    public static void Registration() {
-        System.out.println("Enter your account number:");
-        String accountNumber = sc.next();
-        System.out.println("Enter PIN:");
-        int pin = sc.nextInt();
+    public static void Registration(String accountNumber, int pin) {
 
-        if (hm.containsKey(accountNumber)) {
+        if (accCred.containsKey(accountNumber)) {
             System.out.println("Account Already exist !");
             Verification(accountNumber, pin);
         } else {
-            hm.put(accountNumber, pin);
+            accCred.put(accountNumber, pin);
             System.out.println("Your account has been succesfully registered.");
         }
     }
 
     public static boolean Verification(String accountNumber, int pin) {
         boolean verified = true;
-        if (hm.get(accountNumber) != pin) {
+        if (accCred.get(accountNumber) != pin) {
             verified = false;
             return verified;
         }
         return verified;
+    }
+
+    public static void Deposit(String accountNumber) {
+        System.out.println("Enter the balance you want to deposit.");
+        double deposit = sc.nextDouble();
+        if (accBal.get(accountNumber) == null) {
+            accBal.put(accountNumber, deposit);
+        } else {
+            double Balance = accBal.get(accountNumber);
+            accBal.put(accountNumber, (Balance + deposit));
+        }
+        System.out.println(deposit + " has been successfully deposited in your bank account");
+    }
+
+    public static void checkBal(String accountNumber) {
+        System.out.println("Your account balance is " + accBal.get(accountNumber));
+        if (accBal.get(accountNumber) == null) {
+            System.out.println("Do you want to deposit ? YES = 1 | NO = 0");
+            int option = sc.nextInt();
+
+            if (option == 1)
+                Deposit(accountNumber);
+            return;
+        }
     }
 
     public static void main(String[] args) {
@@ -37,13 +58,14 @@ public class atm {
         System.out.println("2.Existing user");
         int option = sc.nextInt();
 
+        System.out.println("Enter your account number:");
+        String accountNumber = sc.next();
+        System.out.println("Enter PIN:");
+        int pin = sc.nextInt();
+
         if (option == 1) {
-            Registration();
+            Registration(accountNumber, pin);
         } else {
-            System.out.println("Enter your account number:");
-            String accountNumber = sc.next();
-            System.out.println("Enter PIN:");
-            int pin = sc.nextInt();
             boolean verification = Verification(accountNumber, pin);
 
             if (verification == false) {
@@ -53,21 +75,21 @@ public class atm {
         }
         while (true) {
             System.out.println("----------Automated Teller Machine----------");
-            System.out.println("1.Cash Withdraw");
-            System.out.println("2.Check Balance");
-            System.out.println("3.Deposit Money");
+            System.out.println("1.Deposit Money");
+            System.out.println("2.Cash Withdraw");
+            System.out.println("3.Check Balance");
 
             int choice = sc.nextInt();
 
             switch (choice) {
                 case 1:
-
+                    Deposit(accountNumber);
                     break;
                 case 2:
 
                     break;
                 case 3:
-
+                    checkBal(accountNumber);
                     break;
                 default:
                     System.out.println("Invalid Choice.");
